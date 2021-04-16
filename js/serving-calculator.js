5a4ee6838682c87e997ca6dff127c3b6107647ee
elliscode <45629640+elliscode@users.noreply.github.com>
2021-04-15 20:43:34 -0400

    box with boxes, i think input is working

--- js/serving-calculator.js
+++ js/serving-calculator.js
@@ -1,3 +1,6 @@
+const Constants = {};
+Constants.startingWeight = "Starting weight";
+Constants.endingWeight = "Ending weight"
 let copyTimeout = undefined;
 let startGradualFade = function (element) {
     element.style.opacity = 1;
@@ -20,4 +23,47 @@ let fade = function () {
         startGradualFade(fadeDiv);
     }
 };
+let addNew = function () {
+    let servingsDiv = document.getElementById('servings');
+    let calculationDiv = createNewCalculationDiv();
+    servingsDiv.appendChild(calculationDiv);
+}
+let createNewCalculationDiv = function(title, startingWeight, endingWeight) {
+    let div = document.createElement("div");
+    div.classList.add('card');
+    let closeButton = document.createElement('button');
+    closeButton.classList.add('close');
+    closeButton.innerText = '\u00D7';
+    div.appendChild(closeButton);
+    createLabeledInput(div, "Title", title);
+    createLabeledInput(div, "Starting weight", startingWeight, "numeric", 'g');
+    createLabeledInput(div, "Ending weight", endingWeight, "numeric", 'g');
+    createLabeledInput(div, "Serving weight", endingWeight, "numeric", 'g');
+    createLabeledInput(div, "Serving count", endingWeight, "numeric");
+    createLabeledInput(div, "Calories per serving", endingWeight, "numeric");
+    createLabeledInput(div, "Total calories", endingWeight, "numeric");
+    return div;
+}
+let createLabeledInput = function(div, name, value, inputmode, unit) {
+    let subDiv = document.createElement("div");
+    subDiv.classList.add('block');
+    let label = document.createElement("label");
+    label.innerText = name + ": ";
+    let input = document.createElement('input');
+    input.setAttribute('type', 'text');
+    if(value) {
+        input.value = value;
+    }
+    if(inputmode) {
+        input.setAttribute("inputmode", inputmode);
+    }
+    subDiv.appendChild(label);
+    subDiv.appendChild(input);
+    if(unit) {
+        let unitLabel = document.createElement("label");
+        unitLabel.innerText = " " + unit;
+        subDiv.appendChild(unitLabel);
+    }
+    div.appendChild(subDiv);
+}
 fade();
