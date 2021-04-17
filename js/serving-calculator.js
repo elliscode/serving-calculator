8796ea302ded4b798994e18f8b3b1d1ea5f62764
elliscode <45629640+elliscode@users.noreply.github.com>
2021-04-17 14:08:05 -0400

    working with cookies :) test in firefox on your local machine to see the cookies working...

--- js/serving-calculator.js
+++ js/serving-calculator.js
@@ -3,6 +3,8 @@ Constants.startingWeight = "Starting weight";
 Constants.endingWeight = "Ending weight"
 let canUseCookies = false;
 let copyTimeout = undefined;
+let currentData = [];
+let saveFlag = false;
 let startGradualFade = function (element) {
     element.style.opacity = 1;
     clearTimeout(copyTimeout);
@@ -29,7 +31,7 @@ let addNew = function () {
     let calculationDiv = createNewCalculationDiv();
     servingsDiv.appendChild(calculationDiv);
 }
-let createNewCalculationDiv = function(title, startingWeight, endingWeight) {
+let createNewCalculationDiv = function (json) {
     let div = document.createElement("div");
     div.classList.add('card');
     let closeButton = document.createElement('button');
@@ -37,53 +39,124 @@ let createNewCalculationDiv = function(title, startingWeight, endingWeight) {
     closeButton.innerText = '\u00D7';
     closeButton.addEventListener('click', closeCalculationDiv);
     div.appendChild(closeButton);
-    createLabeledInput(div, "Title", title);
-    createLabeledInput(div, "Starting weight", startingWeight, "numeric", 'g');
-    createLabeledInput(div, "Ending weight", endingWeight, "numeric", 'g');
-    createLabeledInput(div, "Serving weight", endingWeight, "numeric", 'g');
-    createLabeledInput(div, "Serving count", endingWeight, "numeric");
-    createLabeledInput(div, "Calories per serving", endingWeight, "numeric");
-    createLabeledInput(div, "Total calories", endingWeight, "numeric");
+    createLabeledInput(div, "Title", json);
+    createLabeledInput(div, "Starting weight", json, "numeric", 'g');
+    createLabeledInput(div, "Ending weight", json, "numeric", 'g');
+    createLabeledInput(div, "Serving weight", json, "numeric", 'g');
+    createLabeledInput(div, "Serving count", json, "numeric");
+    createLabeledInput(div, "Calories per serving", json, "numeric");
+    createLabeledInput(div, "Total calories", json, "numeric");
+    let calculateButton = document.createElement('button');
+    calculateButton.innerText = 'Calculate';
+    calculateButton.style.float = 'right';
+    div.appendChild(calculateButton);
     return div;
 }
 let closeCalculationDiv = function (event) {
     let button = event.target;
     button.parentElement.remove();
+    setSaveFlag();
 }
-let createLabeledInput = function(div, name, value, inputmode, unit) {
+let createLabeledInput = function (div, name, valueJson, inputmode, unit) {
     let subDiv = document.createElement("div");
     subDiv.classList.add('block');
     let label = document.createElement("label");
     label.innerText = name + ": ";
     let input = document.createElement('input');
+    input.setAttribute('field', name);
     input.setAttribute('type', 'text');
-    if(value) {
-        input.value = value;
+    input.addEventListener('change', setSaveFlag);
+    input.addEventListener('input', setSaveFlag);
+    if (valueJson && valueJson[name]) {
+        input.value = valueJson[name];
     }
-    if(inputmode) {
+    if (inputmode) {
         input.setAttribute("inputmode", inputmode);
     }
     subDiv.appendChild(label);
     subDiv.appendChild(input);
-    if(unit) {
+    if (unit) {
         let unitLabel = document.createElement("label");
         unitLabel.innerText = " " + unit;
         subDiv.appendChild(unitLabel);
     }
     div.appendChild(subDiv);
 }
-let cookiesEnabled = function (enabled)  {
+let setSaveFlag = function (event) {
+    saveFlag = true;
+}
+let modifyStoredData = function (event) {
+    if (!saveFlag) {
+        return;
+    }
+    currentData = getArrayOfInputs();
+    writeCookie();
+}
+let getArrayOfInputs = function () {
+    let cardArray = [];
+    let cards = document.getElementsByClassName('card');
+    for (let card of cards) {
+        let cardData = {};
+        let inputs = card.getElementsByTagName('input');
+        for (let input of inputs) {
+            let field = input.getAttribute('field');
+            if(!field || !input.value) {
+                continue;
+            }
+            cardData[field] = input.value;
+        }
+        cardArray.push(cardData);
+    }
+    return cardArray;
+}
+let cookiesEnabled = function (enabled) {
     canUseCookies = enabled;
     let consentBanner = document.getElementById('consent-banner');
     consentBanner.remove();
-    loadCookie();
-}
-let loadCookie = function () {
-    if(document.cookie) {
-        canUseCookies = true;
-        let cookieValues = parseCookie(document.cookie);
+    if (canUseCookies) {
+        loadCookie();
+        writeCookie();
     }
 }
-
+let loadCookie = function () {
+    if (document.cookie) {
+        banner = document.getElementById('consent-banner');
+        banner.remove();
+        canUseCookies = true;
+        let cookieValues = parseCookie(document.cookie);
+        if (cookieValues.hasOwnProperty('currentData')) {
+            currentData = JSON.parse(cookieValues.currentData);
+        }
+        createBoxesFromCookie();
+    }
+}
+let createBoxesFromCookie = function() {
+    let servingsDiv = document.getElementById('servings');
+    for(let boxData of currentData) {
+        let calculationDiv = createNewCalculationDiv(boxData);
+        servingsDiv.appendChild(calculationDiv);
+    }
+}
+let parseCookie = function (cookieString) {
+    let output = {};
+    let cookieParts = cookieString.split(';');
+    for (let cookiePart of cookieParts) {
+        let keyValue = cookiePart.split('=');
+        let key = keyValue[0].trim();
+        let value = keyValue[1].trim();
+        output[key] = value;
+    }
+    return output;
+}
+let writeCookie = function () {
+    let cookieString = '';
+    if (currentData) {
+        cookieString += 'currentData=' + JSON.stringify(currentData) + ';';
+    }
+    cookieString += 'canUseCookies=true;';
+    cookieString += 'SameSite=Strict;';
+    document.cookie = cookieString;
+}
+let periodicSave = setInterval(modifyStoredData, 1000);
 fade();
 loadCookie();
